c9186c0b5b7c03ab31a72d78cddf4e00df3bf08d
Chandan Mandi <itscoderchandan@gmail.com>
2021-07-03 08:13:56 +0530

    assignment 1

---
+++ script.js
@@ -0,0 +1,11 @@
+// First Answer
+var firstName = "Javascript", lastName = "Learner", age = 25;
+    document.getElementById("first").innerHTML = (firstName + " " + lastName + " " + age);
+
+// Second Answer
+var x = 10;
+x += 5;
+    document.getElementById("second").innerHTML = x;
+
+// Third Answer
+
